0dc006dc449aad5c770d46270ce288946d2771ca
bgrayson <bgrayson>
1997-07-10 03:52:06 +0000

    Bumped to 1.4.3beta, since NetBSD swapctl() changes have been added

--- version.cc
+++ version.cc
@@ -1,4 +1,4 @@
   /*  This file is included by xosview.cc, to set up the version
    *  string.  */
-static const char * const versionString = "xosview version 1.4.3alpha";
-static const char * const version_cc_cvsID = "$Id: version.cc,v 1.2 1997/02/14 06:18:39 bgrayson Exp $";
+static const char * const versionString = "xosview version 1.4.3beta";
+static const char * const version_cc_cvsID = "$Id: version.cc,v 1.3 1997/07/10 03:52:06 bgrayson Exp $";
