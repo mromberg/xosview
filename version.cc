ebac4f3883c7aac60ef5542062599071a6b24029
bgrayson <bgrayson>
1997-11-15 03:05:25 +0000

    Changed version to 1.5.0a

--- version.cc
+++ version.cc
@@ -1,4 +1,4 @@
   /*  This file is included by xosview.cc, to set up the version
    *  string.  */
-static const char * const versionString = "xosview version 1.4.4beta";
-static const char * const version_cc_cvsID = "$Id: version.cc,v 1.4 1997/07/10 18:16:27 bgrayson Exp $";
+static const char * const versionString = "xosview version 1.5.0a";
+static const char * const version_cc_cvsID = "$Id: version.cc,v 1.5 1997/11/15 03:05:26 bgrayson Exp $";
