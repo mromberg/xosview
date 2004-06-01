61988764846e9c2147a9099443b0b36b3dbbe0ba
romberg <romberg>
2004-06-01 03:07:40 +0000

    Fix the pagemeter to work with 2.6 kernels (linux)

--- linux/pagemeter.h
+++ linux/pagemeter.h
@@ -1,10 +1,10 @@
-//  
+//
 //  Copyright (c) 1996 by Massimiliano Ghilardi ( ghilardi@cibs.sns.it )
 //
 //  This file may be distributed under terms of the GPL
 //
 //
-// $Id: pagemeter.h,v 1.2 1998/10/15 21:28:18 mromberg Exp $
+// $Id: pagemeter.h,v 1.3 2004/06/01 03:07:41 romberg Exp $
 //
 #ifndef _PAGEMETER_H_
 #define _PAGEMETER_H_
@@ -24,8 +24,12 @@ protected:
   float pageinfo_[2][2];
   int pageindex_;
   float maxspeed_;
+  bool _vmstat;
+  char *_statFileName;
 
   void getpageinfo( void );
+  void getvmpageinfo( void );
+  void updateinfo(void);
 private:
 };
 
