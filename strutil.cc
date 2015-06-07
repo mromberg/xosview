459d1757920ebf055d66704b69ea5d85bd03bdc9
Mike Romberg <mike-romberg@comcast.net>
2015-06-07 10:11:51 -0600

    add string utilities

---
+++ strutil.cc
@@ -0,0 +1,119 @@
+#include "strutil.h"
+#include <algorithm>
+#include <iostream>
+#include <cerrno>
+#include <cassert>
+#include <stdexcept>
+
+namespace util {
+
+const char *WHITE_SPACE = " \f\n\r\t\v";
+
+std::vector<std::string> split(const std::string& s,
+  const std::string& delim, const bool keep_empty)
+    {
+    std::vector<std::string> result;
+    if (delim.empty())
+        {
+        result.push_back(s);
+        return result;
+        }
+    std::string::const_iterator substart = s.begin(), subend;
+    while (true)
+        {
+        subend = std::search(substart, s.end(), delim.begin(), delim.end());
+        std::string temp(substart, subend);
+        if (keep_empty || !temp.empty())
+            result.push_back(temp);
+        if (subend == s.end())
+            break;
+
+        substart = subend + delim.size();
+        }
+
+    return result;
+    }
+
+static void setLocale(void)
+    {
+    static bool first = true;
+    if (first)
+        {
+        std::setlocale(LC_CTYPE, "");  // for s2ws() and ws2s()
+        first = false;
+        }
+    }
+
+std::wstring s2ws(const std::string& s)
+    {
+    setLocale();
+    const char * cs = s.c_str();
+    const size_t wn = std::mbsrtowcs(NULL, &cs, 0, NULL);
+
+    if (wn == size_t(-1))
+        {
+        std::cerr << "Error in mbsrtowcs(): errno=" << errno
+                  << ", str=" << s << std::endl;
+        return L"";
+        }
+
+    std::vector<wchar_t> buf(wn + 1);
+    const size_t wn_again = std::mbsrtowcs(buf.data(), &cs, wn + 1, NULL);
+
+    if (wn_again == size_t(-1))
+        {
+        std::cerr << "Error in mbsrtowcs(): errno=" << errno
+                  << ", str=" << s << std::endl;
+        return L"";
+        }
+
+    assert(cs == NULL); // successful conversion
+
+    return std::wstring(buf.data(), wn);
+    }
+
+std::string ws2s(const std::wstring &s)
+    {
+    setLocale();
+    const wchar_t * cs = s.c_str();
+    const size_t wn = std::wcsrtombs(NULL, &cs, 0, NULL);
+
+    if (wn == size_t(-1))
+        {
+        std::cerr << "Error in wcsrtombs(): errno=" << errno << std::endl;
+        return "";
+        }
+
+    std::vector<char> buf(wn + 1);
+    const size_t wn_again = std::wcsrtombs(buf.data(), &cs, wn + 1, NULL);
+
+    if (wn_again == size_t(-1))
+        {
+        std::cerr << "Error in wcsrtombs(): errno=" << errno << std::endl;
+        return "";
+        }
+
+    assert(cs == NULL); // successful conversion
+
+    return std::string(buf.data(), wn);
+    }
+
+
+
+int stoi(const std::string &str, size_t *, int)
+    {
+    int rval;
+    if (!fstr(str, rval))
+        throw std::invalid_argument("error converting " + str + " to int");
+    return rval;
+    }
+
+float stof(const std::string &str, size_t *)
+    {
+    float rval;
+    if (!fstr(str, rval))
+        throw std::invalid_argument("error converting " + str + " to float");
+    return rval;
+    }
+
+}
---
+++ strutil.h
@@ -0,0 +1,71 @@
+//
+// String manipulations that probably won't be in the standard library
+// before the 32 bit time clock rolls over.
+//
+#ifndef strutil_H
+#define strutil_H
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+namespace util {
+
+extern const char *WHITE_SPACE; // = " \f\n\r\t\v";
+
+inline std::string rstrip(const std::string& s,
+  const std::string& delimiters=WHITE_SPACE)
+    {
+    if (!s.size())
+        return s;
+    return s.substr(0, s.find_last_not_of( delimiters ) + 1);
+    }
+
+inline std::string lstrip(const std::string& s,
+  const std::string& delimiters=WHITE_SPACE)
+    {
+    if (!s.size())
+        return s;
+    return s.substr(s.find_first_not_of(delimiters));
+    }
+
+inline std::string strip(const std::string& s,
+  const std::string& delimiters=WHITE_SPACE)
+    {
+    return lstrip(rstrip(s, delimiters), delimiters);
+    }
+
+template <class X>
+inline std::string repr(const X &x)
+    {
+    std::stringstream os;
+    os << x;
+    return os.str();
+    }
+
+// The stoX functions are in the standard.  But gcc wants
+// you to use -std=c++-11 to use them.  And this has made
+// things explode in the past
+int stoi(const std::string &str, size_t *idx = 0, int base = 10);
+float stof(const std::string &str, size_t *idx = 0);
+
+
+extern std::vector<std::string> split(const std::string& s,
+  const std::string& delim, const bool keep_empty = true);
+
+
+template <class X>
+inline static bool fstr(const std::string &s, X &x)
+    {
+    std::stringstream os(s);
+    os >> x;
+    return os;
+    }
+
+// Deal with those goofy forign characters
+std::wstring s2ws(const std::string& s);
+std::string ws2s(const std::wstring &s);
+
+} // namespace util
+
+#endif
