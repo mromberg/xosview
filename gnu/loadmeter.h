af9503557912a8fd32e08b3ffb704bade78c673b
Mike Romberg <romberg@debian>
2015-09-24 12:12:46 -0400

    loadmeter -> cmeter

--- gnu/loadmeter.h
+++ gnu/loadmeter.h
@@ -5,30 +5,19 @@
 //
 //  This file may be distributed under terms of the GPL
 //
-//  Most of this code was written by Werner Fink <werner@suse.de>
-//  Only small changes were made on my part (M.R.)
-//
 #ifndef LOADMETER_H
 #define LOADMETER_H
 
-#include "fieldmetergraph.h"
+#include "ploadmeter.h"
 
 
-class LoadMeter : public FieldMeterGraph {
+class LoadMeter : public PrcLoadMeter {
 public:
-    LoadMeter( XOSView *parent );
-    ~LoadMeter( void );
-
-    virtual std::string name( void ) const { return "LoadMeter"; }
-    void checkevent( void );
-
-    void checkResources(const ResDB &rdb);
+    LoadMeter( XOSView *parent ) : PrcLoadMeter(parent) {}
 
 protected:
-    void getloadinfo( void );
-    unsigned long procloadcol_, warnloadcol_, critloadcol_;
-private:
-    int warnThreshold, critThreshold, alarmstate, lastalarmstate;
+    virtual uint64_t getCPUSpeed(void) { return 0; }
 };
+
 
 #endif
