0859d141cafba66916288989c37dd72d921b89d6
Mike Romberg <mike-romberg@comcast.net>
2015-10-02 15:39:29 +0000

    fsmeter -> cmeter

--- sunos5/fsmeter.cc
+++ sunos5/fsmeter.cc
@@ -5,98 +5,14 @@
 //  This file may be distributed under terms of the GPL
 //
 #include "fsmeter.h"
-#include "fsutil.h"
-#include "strutil.h"
 
-#include <fstream>
 
 
 static const char * const MOUNT_FNAME = "/etc/mnttab";
 
 
-FSMeter::FSMeter(XOSView *parent, const std::string &path)
-    : FieldMeterGraph(parent, 2, "FS", "USED/FREE", true, true, true),
-      _bgColor(0), _umountColor(0), _path(path){
 
-    legend(_path + ":FREE", ":");
-}
-
-
-FSMeter::~FSMeter(void) {
-}
-
-
-void FSMeter::checkResources(const ResDB &rdb) {
-
-    FieldMeterGraph::checkResources(rdb);
-
-
-    _bgColor = rdb.getColor("filesysBGColor");
-    _umountColor = rdb.getColor("filesysNoneColor");
-    setfieldcolor(0, rdb.getColor("filesysFGColor"));
-    setfieldcolor(1, _bgColor);
-
-    priority_ = util::stoi(rdb.getResource("filesysPriority"));
-    dodecay_ = rdb.isResourceTrue("filesysDecay");
-    useGraph_ = rdb.isResourceTrue("filesysGraph");
-    setUsedFormat(rdb.getResource("filesysUsedFormat"));
-}
-
-
-void FSMeter::checkevent( void ) {
-    total_ = 1.0;
-
-    if (isMount(_path)) {
-        setBGColor(_bgColor);
-
-        // free, total (for root)
-        std::pair<uint64_t, uint64_t> fsSize = util::fs::getSpace(_path, true);
-
-        logDebug << _path << ":\t" << fsSize << std::endl;
-
-        fields_[0] = (float)(fsSize.second - fsSize.first)
-            / (float)fsSize.second;
-        fields_[1] = (float)fsSize.first / (float)fsSize.second;
-        setUsed(fsSize.second - fsSize.first, fsSize.second);
-    }
-    else {
-        fields_[0] = 0;
-        fields_[1] = 1.0;
-        setUsed(0, 1);
-        setBGColor(_umountColor);
-    }
-}
-
-
-void FSMeter::setBGColor(unsigned long c) {
-    if (fieldcolor(1) != c) {
-        fields_[0] = 1.0;
-        fields_[1] = 0;
-        setfieldcolor(1, c);
-        fields_[0] = 0;
-        fields_[1] = 1.0;
-    }
-}
-
-std::vector<std::string> FSMeter::mounts(const ResDB &rdb) {
-    std::string mounts = rdb.getResource("filesysMounts");
-
-    logDebug << "MOUNTS: " << mounts << std::endl;
-
-    if (mounts == "auto")
-        return getAuto();
-
-    // Then filesysMounts is a list of directories
-    std::vector<std::string> rval;
-    rval = util::split(mounts, " ");
-
-    logDebug << "MOUNTS: " << rval << std::endl;
-
-    return rval;
-}
-
-
-std::vector<std::string> FSMeter::getAuto(void) {
+std::vector<std::string> FSMeterFactory::getAuto(void) {
     //  Create a list of entries in mounts where
     //  the type is zfs or tmpfs
 
