82b14b39217713da0dcaec265e5b905814306205
Mike Romberg <romberg@hurd.home.lan>
2016-07-26 04:06:19 -0600

    get_def_pager was not used.

--- gnu/get_def_pager.h
+++
@@ -1,7 +0,0 @@
-//
-//  Copyright (c) 2007 by Samuel Thibault ( samuel.thibault@ens-lyon.org )
-//
-//  This file may be distributed under terms of the GPL
-//
-
-extern mach_port_t get_def_pager(void);
