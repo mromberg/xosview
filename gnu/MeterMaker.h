6b4a80fb46f6d3f43f6fa4d53730d92a5e6d4ee5
romberg <romberg@hurd.home.lan>
2018-01-26 18:34:39 -0700

    unique_ptr

--- gnu/MeterMaker.h
+++ gnu/MeterMaker.h
@@ -1,33 +1,25 @@
 //
-//  Copyright (c) 1994, 1995, 2015
+//  Copyright (c) 1994, 1995, 2015, 2018
 //  by Mike Romberg ( mike-romberg@comcast.net )
 //  2007 by Samuel Thibault ( samuel.thibault@ens-lyon.org )
 //
 //  This file may be distributed under terms of the GPL
 //
-#ifndef METERMAKER_H
-#define METERMAKER_H
+#ifndef MeterMaker_h
+#define MeterMaker_h
 
-#include "rdb.h"
+#include "cmetermaker.h"
 
-#include <vector>
-
-
-class Meter;
+class ResDB;
 
 
-class MeterMaker {
+
+class MeterMaker : public ComMeterMaker {
 public:
-    MeterMaker(void);
-
-    std::vector<Meter *> makeMeters(const ResDB &rdb);
+    mlist makeMeters(const ResDB &rdb);
 
 private:
-    std::vector<Meter *> _meters;
-
-    void cpuFactory(const ResDB &rdb);
-    void getRange(const ResDB &rdb, const std::string &resource,
-      size_t cpuCount, size_t &start, size_t &end) const;
+    void cpuFactory(const ResDB &rdb, mlist &meters) const;
 };
 
 #endif
