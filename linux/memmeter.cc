b799bf7195545c09735bab4f48ac85bf85074b67
Mike Romberg <mike-romberg@comcast.net>
2018-04-04 00:14:43 -0600

    refactor

--- linux/memmeter.cc
+++ linux/memmeter.cc
@@ -7,18 +7,10 @@
 
 #include "memmeter.h"
 
-#include <fstream>
-#include <iomanip>
-
-
-static const char * const MEMFILENAME = "/proc/meminfo";
-
 
 
 MemMeter::MemMeter(void)
-    : FieldMeterGraph(5, "MEM", "USED/BUFF/CACHE/SCACHE/FREE") {
-
-    initLineInfo();
+    : PrcMemMeter(5, "USED/BUFF/CACHE/SCACHE/FREE") {
 }
 
 
@@ -33,63 +25,7 @@ void MemMeter::checkResources(const ResDB &rdb) {
 }
 
 
-void MemMeter::checkevent(void) {
-    getmeminfo();
-    // for debugging (see below)
-    const float TOMEG = 1.0 / 1024.0 / 1024.0;
-    logDebug << std::setprecision(1) << std::fixed
-             << "t " << _total * TOMEG << " "
-             << "used "    << _fields[0] * TOMEG << " "
-             << "buf "     << _fields[1] * TOMEG << " "
-             << "cache "   << _fields[2] * TOMEG << " "
-             << "swcache " << _fields[3] * TOMEG << " "
-             << "free "    << _fields[4] * TOMEG
-             << std::endl;
-}
-
-
-void MemMeter::getmeminfo(void) {
-    getmemstat(MEMFILENAME, _MIlineInfos);
-
-    _fields[0] = _total - _fields[4] - _fields[3] - _fields[2] - _fields[1];
-
-    if (_total)
-        FieldMeterDecay::setUsed (_total - _fields[3] - _fields[4], _total);
-}
-
-
-std::vector<MemMeter::LineInfo> MemMeter::findLines(
-    const std::vector<LineInfo> &tmplate, const std::string &fname) const {
-
-    std::ifstream meminfo(fname);
-    if (!meminfo) {
-        logFatal << "Can not open file : " << fname << std::endl;
-    }
-
-    std::vector<LineInfo> rval(tmplate.size());
-
-    // Get the info from the "standard" meminfo file.
-    size_t lineNum = 0;
-    size_t inum = 0;  // which info are we going to insert
-    while (!meminfo.eof()) {
-        std::string buf;
-        std::getline(meminfo, buf);
-        lineNum++;
-
-        for (const auto &li : tmplate)
-            if (li.id() == buf.substr(0, li.id().size())) {
-                logDebug << "FOUND line: " << buf << std::endl;
-                rval[inum] = li;
-                rval[inum].line(lineNum);
-                inum++;
-            }
-    }
-
-    return rval;
-}
-
-
-void MemMeter::initLineInfo(void) {
+std::vector<PrcMemMeter::LineInfo> MemMeter::newLineInfos(void) {
     std::vector<LineInfo> infos = {
         LineInfo("MemTotal", &_total),
         LineInfo("Buffers", &_fields[1]),
@@ -98,39 +34,13 @@ void MemMeter::initLineInfo(void) {
         LineInfo("MemFree", &_fields[4])
     };
 
-    _MIlineInfos = findLines(infos, MEMFILENAME);
+    return infos;
 }
 
 
-void MemMeter::getmemstat(const std::string &fname,
-  std::vector<LineInfo> &infos) const {
+void MemMeter::setFields(void) {
+    _fields[0] = _total - _fields[4] - _fields[3] - _fields[2] - _fields[1];
 
-    std::ifstream meminfo(fname);
-    if (!meminfo) {
-        logFatal << "Can not open file : " << fname << std::endl;
-    }
-
-    // Get the info from the "standard" meminfo file.
-    size_t lineNum = 0;
-    size_t fcount = 0; // found count
-    while (!meminfo.eof()) {
-        std::string buf;
-        std::getline(meminfo, buf);
-        lineNum++;
-        for (auto &li : infos)
-            if (li.line() == lineNum) {
-                std::istringstream line(buf);
-                size_t val;
-                std::string ignore;
-                line >> ignore >> val;
-                // All stats are in KB.
-                // Multiply by 1024 bytes per K
-                li.setVal(val * 1024.0);
-                fcount++;
-                break;
-            }
-
-        if (fcount >= infos.size())
-            break;
-    }
+    if (_total)
+        setUsed(_total - _fields[3] - _fields[4], _total);
 }
--- linux/memmeter.h
+++ linux/memmeter.h
@@ -7,48 +7,19 @@
 #ifndef memmeter_h
 #define memmeter_h
 
-#include "fieldmetergraph.h"
+#include "pmemmeter.h"
 
 
 
-class MemMeter : public FieldMeterGraph {
+class MemMeter : public PrcMemMeter {
 public:
     MemMeter(void);
 
-    virtual std::string resName(void) const override { return "mem"; }
-    virtual void checkevent(void) override;
-
     virtual void checkResources(const ResDB &rdb) override;
 
-private:
-    class LineInfo {
-    public:
-        LineInfo(const std::string &id, float *val)
-            : _line(-1), _id(id), _val(val) {}
-        LineInfo(void) : _line(-1), _val(nullptr) {};
-
-        size_t line(void) const { return _line; }
-        void line(size_t l) { _line = l; }
-        const std::string &id(void) const { return _id; }
-        size_t idlen(void) const { return _id.size(); }
-
-        void setVal(double val) { *_val = val; }
-
-    private:
-        size_t _line;
-        std::string _id;
-        float *_val;  // pointer into _fields array
-    };
-
-    std::vector<LineInfo> _MIlineInfos;
-    std::vector<LineInfo> _MSlineInfos;
-
-    void getmeminfo(void);
-    void initLineInfo(void);
-    std::vector<LineInfo> findLines(const std::vector<LineInfo> &tmplate,
-      const std::string &fname) const;
-    void getmemstat(const std::string &fname,
-      std::vector<LineInfo> &infos) const;
+protected:
+    virtual std::vector<LineInfo> newLineInfos(void) override;
+    virtual void setFields(void) override;
 };
 
 
